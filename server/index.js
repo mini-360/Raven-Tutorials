2f775fe25e41672cd88b430f79595bd2cc4a3458
mini-360 <rajabhay56999@gmail.com>
2024-09-20 19:36:28 +0530

    database connection created

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+connectDB().then(() => {
+  app.on("error", (error) => {
+    console.log("ERROR", error);
+    throw error;
+  });
+  app
+    .listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+    .catch((error) => {
+      console.log("MongoDB connection failed ", error);
+    });
+});
